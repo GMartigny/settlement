85968eb1be77e6dd7649f020657e49da5477dce1
Guillaume Martigny <guillaume.martigny@gmail.com>
2018-01-04 09:56:17 +0100

    Refactor color tests

--- tests/colorsTest.js
+++ tests/colorsTest.js
@@ -1,22 +1,34 @@
 describe("Test all color helpers", function colorsUtilsDescribe () {
 
+    var separator = ", ";
+    var blue = {
+        hexa: "#0AF",
+        hexaLong: "#00AAFF",
+        rgb: [0x0, 0xAA, 0xFF].join(separator),
+        grey: "#8E8E8E"
+    };
+    var red = {
+        hexa: "#E5421F",
+        rgb: [0xE5, 0x42, 0x1F].join(separator),
+        grey: "#6D6D6D"
+    };
+
     it ("fade", function colorsFade () {
 
-        var color = "#0AF";
-        expect(ColorsUtils.fade(color, 0.5)).toEqual("rgba(0, 170, 255, 0.5)");
-        expect(ColorsUtils.fade(color, 1)).toEqual("#00AAFF");
+        var color = blue.hexa;
+        expect(ColorsUtils.fade(color, 0.5)).toEqual("rgba(" + blue.rgb + ", 0.5)");
+        expect(ColorsUtils.fade(color, 1)).toEqual(blue.hexaLong);
 
-        color = "#E5421F";
-        expect(ColorsUtils.fade(color, 0.5)).toEqual("rgba(229, 66, 31, 0.5)");
-        expect(ColorsUtils.fade(color, 1)).toEqual("#E5421F");
+        color = red.hexa;
+        expect(ColorsUtils.fade(color, 0.5)).toEqual("rgba(" + red.rgb + ", 0.5)");
+        expect(ColorsUtils.fade(color, 1)).toEqual(red.hexa);
 
     });
 
     it("grey", function colorsGrey () {
 
-        expect(ColorsUtils.grey("#0AF")).toEqual("#8E8E8E");
-        expect(ColorsUtils.grey("#E5421F")).toEqual("#6D6D6D");
-        expect(ColorsUtils.grey("#000")).toEqual("#000000");
+        expect(ColorsUtils.grey(blue.hexa)).toEqual(blue.grey);
+        expect(ColorsUtils.grey(red.hexa)).toEqual(red.grey);
 
     });
 
@@ -24,58 +36,58 @@ describe("Test all color helpers", function colorsUtilsDescribe () {
 
         it("fade", function () {
 
-            var color = new Color("#0AF");
-            expect(color.fade(0.5).toString()).toEqual("rgba(0, 170, 255, 0.5)");
-            expect(color.fade(0).toString()).toEqual("rgba(0, 170, 255, 0)");
+            var color = new Color(blue.hexa);
+            expect(color.fade(0.5).toString()).toEqual(`rgba(${blue.rgb}, 0.5)`);
+            expect(color.fade(0).toString()).toEqual(`rgba(${blue.rgb}, 0)`);
 
-            expect(color.fade(1).toString()).toEqual("#00AAFF");
+            expect(color.fade(1).toString()).toEqual(blue.hexaLong);
 
         });
 
         it("grey", function () {
 
-            var color = new Color("#0AF");
-            expect(color.grey().toString()).toEqual("#8E8E8E");
+            var color = new Color(blue.hexa);
+            expect(color.grey().toString()).toEqual(blue.grey);
 
-            color = new Color("#E5421F");
-            expect(color.grey().toString()).toEqual("#6D6D6D");
+            color = new Color(red.hexa);
+            expect(color.grey().toString()).toEqual(red.grey);
 
         });
 
         it("toHexa", function () {
 
-            var color = new Color("#0AF");
-            expect(color.toHexa()).toEqual("#00AAFF");
+            var color = new Color(blue.hexa);
+            expect(color.toHexa()).toEqual(blue.hexaLong);
 
-            color = new Color("#E5421F");
-            expect(color.fade(0.3).toHexa()).toEqual("#E5421F");
+            color = new Color(red.hexa);
+            expect(color.fade(0.3).toHexa()).toEqual(red.hexa);
 
         });
 
         it("toRGB", function () {
 
-            var color = new Color("#0AF");
-            expect(color.toRGB()).toEqual("rgb(0, 170, 255)");
+            var color = new Color(blue.hexa);
+            expect(color.toRGB()).toEqual(`rgb(${blue.rgb})`);
 
-            color = new Color("#E5421F");
-            expect(color.fade(0.3).toRGB()).toEqual("rgb(229, 66, 31)");
+            color = new Color(red.hexa);
+            expect(color.fade(0.3).toRGB()).toEqual(`rgb(${red.rgb})`);
 
         });
 
         it("toRGBA", function () {
 
-            var color = new Color("#0AF");
-            expect(color.toRGBA()).toEqual("rgba(0, 170, 255, 1)");
+            var color = new Color(blue.hexa);
+            expect(color.toRGBA()).toEqual(`rgba(${blue.rgb}, 1)`);
 
-            color = new Color("#E5421F");
-            expect(color.fade(0.3).toRGBA()).toEqual("rgba(229, 66, 31, 0.3)");
+            color = new Color(red.hexa);
+            expect(color.fade(0.3).toRGBA()).toEqual(`rgba(${red.rgb}, 0.3)`);
 
         });
 
         it("toString", function () {
 
-            var color = new Color("#0AF");
-            expect("color " + color).toEqual("color #00AAFF"); // Magic coercion
+            var color = new Color(blue.hexa);
+            expect("color " + color).toEqual(`color ${blue.hexaLong}`); // Magic coercion with concatenation
 
         });
 
